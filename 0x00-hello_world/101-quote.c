8f182e1ef4ca94e86ac5a4239c779a2e9415b12a
char141 <charitymakokha389@gmail.com>
2022-09-08 10:08:30 -0700

    Print to std error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,9 @@
 #include <unistd.h>
 
 /**
- *
- *  * main - Entry point
- *
- *   * Return: Always 1 (Success)
- *
- *    */
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
 
 int main(void)
 {
