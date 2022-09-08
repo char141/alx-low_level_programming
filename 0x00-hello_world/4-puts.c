057a7d4e67c5a464cfc5cfac5d89228733414fd7
char141 <52075284+char141@users.noreply.github.com>
2022-09-08 19:08:09 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
  */
 int main(void) /* Entry point of the program*/
 {
-		puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
