3518f0a7ea73a59067478a20bb454235dcded06e
char141 <52075284+char141@users.noreply.github.com>
2022-09-09 16:14:45 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,9 +6,10 @@
 int main(void)
 {
 	char alphabet;
-	for(alphabet = 'a' ; alphabet <='z' ; alphabet++)
+	
+	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
 		putchar (alphabet);
-	for(alphabet = 'A' ; alphabet <='Z' ; alphabet++)
+	for (alphabet = 'A' ; alphabet <= 'Z' ; alphabet++)
 		putchar (alphabet);
 	putchar ('\n');
 	return (0);
