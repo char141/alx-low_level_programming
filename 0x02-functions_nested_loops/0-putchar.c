c753a9b1fd919d3cbb2357f05b7fc74d2e09dcae
char141 <charitymakokha389@gmail.com>
2022-09-12 11:56:27 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main -   prints _putchar
+ * result: Always 0
+*/
+int main(void)
+{
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
