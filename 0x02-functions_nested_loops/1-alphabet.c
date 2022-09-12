d6a87f42c66957c7d97f11120789e2cf04e65aff
char141 <charitymakokha389@gmail.com>
2022-09-12 12:05:15 -0700

    Prints the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * Main - Prints the alphabet in lowercase using _putchar
+ * Result: Success
+*/
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+}
