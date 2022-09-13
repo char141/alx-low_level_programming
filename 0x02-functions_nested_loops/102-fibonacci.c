6a7db10322e90e4655b24ba8f0ce9ef010f2b804
char141 <charitymakokha389@gmail.com>
2022-09-13 06:25:25 -0700

    first fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include "stio.h"
+#include "stdlib.h"
+/**
+ * main - prints the first 50 fibbonachi numbers.
+ * Result: always 0
+*/
+int main(void)
+{
+	int placeholder;
+	unsigned long term1, term2, nextterm;
+
+	for (placeholder = 0; placeholder <= 50; placeholder++)
+	{
+		nextterm = term1 + term2;
+		printf("%d", nextterm);
+
+		term1 = term2;
+		term2 = nextterm;
+
+		if (placeholder == 49)
+			printf("\n");
+		else
+			printf(",");
+	}
+	return (0);
+}
+
