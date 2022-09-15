9589117dae8ed76d038ddee809c830f02db94829
char141 <charitymakokha389@gmail.com>
2022-09-15 11:07:51 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - function that checks for uppercase character.
+ * Return: 0
+*/
+
+int main(void)
+{
+	int place = 1;
+
+	while (place <= 100)
+	{
+		if (place % 3 == 0 && place % 5 == 0)
+			printf("FizzBuzz ");
+		else if (place % 5 == 0)
+		{
+			if (place == 100)
+			{
+				printf("Buzz");
+				printf("\n");
+			}
+			else
+				printf("Buzz ");
+		}
+		else if (place % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", place);
+		h++;
+	}
+	return (0);
+}
