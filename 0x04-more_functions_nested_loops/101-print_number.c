4b5b326a987fc7b845c6965494ac7ede9a63bc13
char141 <charitymakokha389@gmail.com>
2022-09-15 14:33:54 -0700

    prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_number - a function that prints an integer
@@ -8,5 +8,27 @@
 
 void print_number(int n)
 {
-	return (0);
+	unsigned int num1, num2, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		num1 = n * -1;
+	}
+	else
+	{
+		num1 = n;
+	}
+
+	num2 = num1;
+	count = 1;
+	while (num2 > 9)
+	{
+		num2 /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((num1 / count) % 10) + 48);
+	}
 }
