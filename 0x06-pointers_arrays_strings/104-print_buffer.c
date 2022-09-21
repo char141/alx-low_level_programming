f9812f86aad0d890b4cd518bc1c12ffaaf7dc344
char141 <charitymakokha389@gmail.com>
2022-09-21 10:13:46 -0700

    print s bytes of a buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_line - function to print bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of a buffer to print
+ * @l: line of a buffer to print
+ * Return: 0 on success
+ */
+void print_line(char *c, int s, int l)
+{
+	int j, k;
+
+	for (j = 0; j <= 9; j++)
+	{
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
+		else
+			printf(" ");
+		if (j % 2)
+			_putchar(' ');
+	}
+	for (k = 0; k <= s; k++)
+	{
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			_putchar(c[l * 10 + k]);
+		else
+			_putchar('.');
+	}
+}
+
+/**
+ * print_buffer - function to print a buffer
+ * @b: pointer to a buffer
+ * @size: buffer size
+ * Return: Always 0
+ */
+void print_buffer(char *b, int size)
+{
+	int i;
+
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	{
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
+		{
+			print_line(b, 9, i);
+		}
+		else
+		{
+			print_line(b, size % 10 - 1, i);
+		}
+		_putchar('\n');
+	}
+	if (size == 0)
+		_putchar('\n');
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This direcotory contains c programs on pointers, strings and arrays
