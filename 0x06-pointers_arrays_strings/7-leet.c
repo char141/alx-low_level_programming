2581d1118cd72b45e31446ead6dfe701218a61c5
char141 <charitymakokha389@gmail.com>
2022-09-22 00:17:08 -0700

    encode a string in 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *  leet - function to encode a string into 1337
+ *  @s: string to be encoded
+ *  Return: success
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char a[] = "aeotlAEOTL";
+	char b[] = "4307143071";
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; a[j], j++)
+		{
+			if (s[i] == a[j])
+				s[i] = b[j];
+		}
+	}
+	return (s);
+}
