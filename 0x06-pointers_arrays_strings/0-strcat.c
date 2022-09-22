d822a75cfcb0a4301b6b7036db2e0d354f48e37a
char141 <charitymakokha389@gmail.com>
2022-09-22 01:17:35 -0700

    function that concacenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcat - function to append a string to another
+ * @dest: append to
+ * @src: append from
+ * Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (dest[i] = src[j])
+	{
+		i++;
+		j++;
+	}
+	return (dest);
+}
