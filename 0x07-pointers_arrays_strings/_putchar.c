1ddc9e223bbd4a5af0d7113fdd256a8eec0ff9a8
char141 <charitymakokha389@gmail.com>
2022-09-26 08:50:26 -0700

    function to print a byte filled memory

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * _putchar - function to print an integer
+ * @c: character to be printed
+ * Return: 0 on success or -1
+ */
+int putchar(char c)
+{
+	return (write(i, &c, 1));
+}
