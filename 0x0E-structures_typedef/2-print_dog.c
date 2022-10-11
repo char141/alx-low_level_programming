c55e09a8943db2fc995cb75263f50e3e6088c1a6
char141 <charitymakokha389@gmail.com>
2022-10-11 02:46:17 -0700

    function to print the contents of a strct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_dog - function to print the contents of a struct dog
+ * @d: structure to be printed
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	if (d->age < 0)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %s\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: %s\n", d->owner);
+}
