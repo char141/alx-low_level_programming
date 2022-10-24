4d0c160b639af3614b2e67828859107a95af8d35
char141 <charitymakokha389@gmail.com>
2022-10-24 04:31:09 -0700

    Adds a node at the begining of the list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * add_node - adds a node at the begining of the list
+ * @head: pointer to a list_t pointer that points to the head struct
+ * @str: string to add as node
+ * Return: pointer to new head of list, NULL on failure
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+	int len = 0;
+
+	temp = *head;
+	*head = malloc(sizeof(list_t));
+	if (!*head)
+		return (NULL);
+	while (str[len])
+		len++;
+	(*head)->len = len;
+	if (str)
+	{
+		(*head)->str = strdup(str);
+		if (!(*head)->str)
+		{
+			 free(*head);
+			 return (NULL);
+		}
+	}
+	else
+		(*head)->str = NULL;
+	(*head)->next = temp;
+	return (*head);
+}
