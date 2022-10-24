3c87f7fd96f4332ed97102fc02cb70a1dc3feffe
char141 <charitymakokha389@gmail.com>
2022-10-24 03:57:39 -0700

    Prints the number of elements in a list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * list_len - prints the number of elements in a list
+ * @h: the list
+ * Return: number of elements in the list
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *temp;
+	size_t nodes = 0;
+
+	temp = h;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		nodes++;
+	}
+	return (nodes);
+}
