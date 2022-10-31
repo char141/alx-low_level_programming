f8a50fe9d941578fd8ecbdbf7d3e2cd37702a7e7
char141 <charitymakokha389@gmail.com>
2022-10-31 04:36:48 -0700

    sets the value of a bit to 0 at a given index.

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,9 +2,9 @@
 /**
  * clear_bit - sets a bit at given index to 0
  *
- * @n: number to set bit in
- * 
- * @ndex: index to set bit at
+ * @n: number to set bit int
+ *
+ * @index: index to set bit at
  *
  * Return: 1 if it worked, or -1 on error
  */
